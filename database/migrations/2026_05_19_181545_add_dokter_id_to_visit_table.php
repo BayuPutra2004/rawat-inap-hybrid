3d387f1c24ff5da5943ab150e0b8993b140b85f6
Bayu Angkasa Putra <artisekarang@gmail.com>
2026-05-20 01:16:09 +0700

    add dokter_id to visit table

---
+++ database/migrations/2026_05_19_181545_add_dokter_id_to_visit_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('visit', function (Blueprint $table) {
+
+            $table->foreignId('dokter_id')
+                  ->nullable()
+                  ->constrained('users')
+                  ->nullOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('visit', function (Blueprint $table) {
+
+            $table->dropForeign(['dokter_id']);
+
+            $table->dropColumn('dokter_id');
+
+        });
+    }
+};
